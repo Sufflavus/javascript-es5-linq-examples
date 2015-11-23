2f7cf575ec1065c6783806aacbec738b20963897
Tania Shatilova <tatashat@gmail.com>
2015-11-24 00:44:35 +0300

    * GroupBy simple 1

--- src/samples/grouping/040-group-by-simple-1.js
+++ src/samples/grouping/040-group-by-simple-1.js
@@ -1,31 +1,6 @@
 (function() {
     var numbers = [5, 4, 1, 3, 9, 8, 6, 7, 2, 0]; 
 
-    var numberGroups = numbers.reduce(function(dict, item) {
-        var key = item % 5;
-        if(!dict[key]){
-            dict[key] = [];
-        }
-        dict[key].push(item);
-        return dict;
-    },{});
-
-    var result = Object.keys(numberGroups).map(function(key, index) {
-       return {remainder: key, numbers: numberGroups[key] };
-    });
-
-    result.forEach(function(g) {
-        console.log("Numbers with a remainder of {0} when divided by 5:", g.remainder);
-        g.numbers.forEach(function(n) {
-            console.log(n);
-        })
-    });
-
-})();
-
-(function() {
-    var numbers = [5, 4, 1, 3, 9, 8, 6, 7, 2, 0]; 
-
     var numberGroups = numbers.reduce(function(array, item) {
         var key = item % 5;
 
@@ -38,12 +13,12 @@
         }
 
         return array;
-    },[]).map(function(g, index) {
+    }, []).map(function(g, index) {
        return {remainder: g.key, numbers: g.values };
     });
 
     numberGroups.forEach(function(g) {
-        console.log("Numbers with a remainder of {0} when divided by 5:", g.remainder);
+        console.log("Numbers with a remainder of " + g.remainder + " when divided by 5:");
         g.numbers.forEach(function(n) {
             console.log(n);
         })
